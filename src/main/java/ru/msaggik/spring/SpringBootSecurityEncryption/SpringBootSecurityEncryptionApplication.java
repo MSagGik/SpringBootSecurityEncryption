3dbd4a0f862cfb4aaac9650e5f4a3b824834496e
MSagGik <Sagaciyang@yandex.ru>
2022-08-29 13:31:42 +0300

    Commit 290822

---
+++ src/main/java/ru/msaggik/spring/SpringBootSecurityEncryption/SpringBootSecurityEncryptionApplication.java
@@ -0,0 +1,13 @@
+package ru.msaggik.spring.SpringBootSecurityEncryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityEncryptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/msaggik/spring/SpringBootSecurityEncryption/SpringBootSecurityEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.msaggik.spring.SpringBootSecurityEncryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
